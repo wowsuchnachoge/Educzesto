025120433970b1f6c9d0f16e83122da52fea4df5
wowsuchnachoge <ignacio.hernandez.monroy@gmail.com>
2021-07-05 17:46:58 -0500

    Updated UI and functionality

--- educzesto-mailer/dbConf.php
+++ educzesto-mailer/dbConf.php
@@ -1,6 +1,6 @@
 <?php
-$DB_CONF['server'] = 'localhost';
+$DB_CONF['server'] = 'localhost'; //173.201.185.124
 $DB_CONF['user'] = 'educzesto';
 $DB_CONF['password'] = 'ServicioSocial2021';
-$DB_CONF['database'] = 'sistemaAlumnas';
+$DB_CONF['database'] = 'sistema_educzesto';
 ?>
